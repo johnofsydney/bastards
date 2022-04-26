dbb918ae4eb283dd4912176942f0ead16222d382
John Coote <john_coote@rocketmail.com>
2022-04-26 21:17:14 +1000

    made all lists of candidates into tables

--- app/controllers/qualification_levels_controller.rb
+++ app/controllers/qualification_levels_controller.rb
@@ -1,5 +1,5 @@
 class QualificationLevelsController < ApplicationController
-  before_action :set_qualification_level, only: %i[ show edit update destroy ]
+  before_action :set_qualification_level, only: %i[ show ]
 
   # GET /qualification_levels or /qualification_levels.json
   def index
@@ -20,61 +20,10 @@ class QualificationLevelsController < ApplicationController
   def show
   end
 
-  # GET /qualification_levels/new
-  def new
-    @qualification_level = QualificationLevel.new
-  end
-
-  # GET /qualification_levels/1/edit
-  def edit
-  end
-
-  # POST /qualification_levels or /qualification_levels.json
-  def create
-    @qualification_level = QualificationLevel.new(qualification_level_params)
-
-    respond_to do |format|
-      if @qualification_level.save
-        format.html { redirect_to qualification_level_url(@qualification_level), notice: "Qualification level was successfully created." }
-        format.json { render :show, status: :created, location: @qualification_level }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @qualification_level.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /qualification_levels/1 or /qualification_levels/1.json
-  def update
-    respond_to do |format|
-      if @qualification_level.update(qualification_level_params)
-        format.html { redirect_to qualification_level_url(@qualification_level), notice: "Qualification level was successfully updated." }
-        format.json { render :show, status: :ok, location: @qualification_level }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @qualification_level.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /qualification_levels/1 or /qualification_levels/1.json
-  def destroy
-    @qualification_level.destroy
-
-    respond_to do |format|
-      format.html { redirect_to qualification_levels_url, notice: "Qualification level was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_qualification_level
-      @qualification_level = QualificationLevel.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def qualification_level_params
-      params.require(:qualification_level).permit(:level, :candidate_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_qualification_level
+    @qualification_level = QualificationLevel.find(params[:id])
+  end
 end
