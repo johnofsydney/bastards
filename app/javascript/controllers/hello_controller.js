a69642b035a4f54a17abeb0f3f5a8cce8bb996f8
David Coote <david.coote@terem.com.au>
2022-05-11 17:42:24 +0930

    Add words and some styling for the home page

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,3 +5,12 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
+
+$('.read-more a').click(function(e) {
+  console.log('clicked');
+  $(this).closest('.expandable').toggleClass('expanded');
+  $(this).text(function(i, text){
+    return text === "Read More" ? "Read Less" : "Read More";
+  })
+  e.preventDefault();
+});
