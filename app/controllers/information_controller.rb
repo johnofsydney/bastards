d3c602373a630a7a9dbf09679423f706edc271f4
David Coote <david.coote@terem.com.au>
2022-05-11 20:53:06 +0930

    text changes

---
+++ app/controllers/information_controller.rb
@@ -0,0 +1,7 @@
+class InformationController < ApplicationController
+  # GET /information or /information.json
+  def index
+
+  end
+
+end
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -5,10 +5,10 @@ export default class extends Controller {
     this.element.textContent = "Hello World!"
   }
 }
-
-$('.read-more a').click(function(e) {
-  console.log('clicked');
-  $(this).closest('.expandable').toggleClass('expanded');
+//
+$('a.read-more').click(function(e) {
+  // @TODO: Only works for one element on page.
+  $('.expandable').toggleClass('expanded');
   $(this).text(function(i, text){
     return text === "Read More" ? "Read Less" : "Read More";
   })
